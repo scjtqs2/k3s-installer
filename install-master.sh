6e0e704a8e3acd6089d73a711ced18da29258442
scjtqs <scjtqs@qq.com>
2022-11-21 01:27:48 +0800

    cn

--- install-master.sh
+++ install-master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # master节点初始化安装
-curl -sfL https://get.k3s.io | K3S_TOKEN=myPassw0rd123456 INSTALL_K3S_VERSION="v1.23.14+k3s1" K3S_CONFIG_FILE=`pwd`/config.yaml INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=1-65535  --write-kubeconfig ~/.kube/config --write-kubeconfig-mode 666 --disable traefik"  sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_MIRROR=cn K3S_TOKEN=myPassw0rd123456 INSTALL_K3S_VERSION="v1.23.14+k3s1" K3S_CONFIG_FILE=`pwd`/config.yaml INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=1-65535  --write-kubeconfig ~/.kube/config --write-kubeconfig-mode 666 --disable traefik"  sh -s -
