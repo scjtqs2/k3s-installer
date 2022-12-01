e5143370435785fe4b5ed70aa25aecff8ba4721a
scjtqs <scjtqs@qq.com>
2022-12-01 12:21:27 +0800

    update: 对墙内下载github增加代理。改了下原始的安装脚本。 对应cnproxy 后缀的脚步

---
+++ install-node_cnproxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# node节点安装 并加入到master集群
+#!/bin/bash
+cat k3s.sh | INSTALL_K3S_MIRROR=cn K3S_TOKEN=myPassw0rd123456 INSTALL_K3S_VERSION="v1.23.14+k3s1" K3S_URL="https://10.0.0.122:6443"  sh -s -
