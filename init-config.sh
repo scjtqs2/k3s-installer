3dc49d4ff5e34ef0487e89b4f881eaeaeec348a2
scjtqs <scjtqs@qq.com>
2024-03-02 22:04:13 +0800

    更新版本

--- init-config.sh
+++ init-config.sh
@@ -34,13 +34,13 @@ sudo sysctl --system
 
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 ## 安装docker
-echo "开始安装docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install containerd.io
+#echo "开始安装docker"
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update
+#sudo apt-get install containerd.io
 
 # 不需要完整的docker。只需要有containerd就行
 
--- install-master_dual-stack_cnproxy.sh
+++ install-master_dual-stack_cnproxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # master节点初始化安装
-cat k3s.sh | INSTALL_K3S_MIRROR=cn K3S_TOKEN=myPassw0rd123456 INSTALL_K3S_VERSION="v1.23.14+k3s1" K3S_CONFIG_FILE=`pwd`/config_dual-stack.yaml INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=1-65535  --write-kubeconfig ~/.kube/config --write-kubeconfig-mode 666 --disable traefik --flannel-ipv6-masq"  sh -s -
+cat k3s.sh | INSTALL_K3S_MIRROR=cn K3S_TOKEN=myPassw0rd123456 INSTALL_K3S_VERSION="v1.26.6+k3s1" K3S_CONFIG_FILE=`pwd`/config_dual-stack.yaml INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=1-65535  --write-kubeconfig ~/.kube/config --write-kubeconfig-mode 666 --disable traefik --flannel-ipv6-masq --disable metrics-server"  sh -s -
